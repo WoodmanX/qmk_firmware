9f67676f6a8d06931fab640709109a3e029c32f6
WoodmanX <WoodmanX@gmail.com>
2024-09-18 22:40:25 +0200

    Activate Numlock state Setup scrollock state Create own keymap

--- keyboards/keychron/v6/iso_encoder/config.h
+++ keyboards/keychron/v6/iso_encoder/config.h
@@ -38,4 +38,9 @@
 #define CAPS_LOCK_LED_INDEX 60
 
 /* Enable NUM_LOCK_LED_INDEX */
-// #define NUM_LOCK_LED_INDEX 37
+#define NUM_LOCK_LED_INDEX 37
+
+/* Enable scroll-lock LED*/
+#define SCROLL_LOCK_LED_INDEX 14
+
+
